644c825ca4d55b4d7996f839fed5cb2eb741f7ca
K4rt0 <duongkhoanam147@gmail.com>
2024-06-22 23:15:07 +0700

    Category Api Final

--- src/main/java/com/stores/stridestar/access/SecurityConfig.java
+++ src/main/java/com/stores/stridestar/access/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,52 +19,52 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-        @Bean
-        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-                return http
-                                .authorizeHttpRequests(requests -> requests
-                                                .requestMatchers("/", "/home", "/main-site/**", "/fontawesome/**")
-                                                .permitAll()
-                                                /*
-                                                 * .requestMatchers("/admin/**", "/admin/**")
-                                                 * .hasRole("ADMIN") // Use hasRole instead of hasAnyAuthority
-                                                 * .requestMatchers("/api/**")
-                                                 * .permitAll()
-                                                 */
-                                                .anyRequest().authenticated())
-                                .formLogin(formLogin -> formLogin
-                                                .loginPage("/login")
-                                                .loginProcessingUrl("/login")
-                                                .defaultSuccessUrl("/", true)
-                                                .failureUrl("/login?error")
-                                                .permitAll())
-                                .logout(logout -> logout
-                                                .logoutUrl("/logout")
-                                                .logoutSuccessUrl("/login")
-                                                .deleteCookies("JSESSIONID")
-                                                .invalidateHttpSession(true)
-                                                .clearAuthentication(true)
-                                                .permitAll())
-                                .sessionManagement(sessionManagement -> sessionManagement
-                                                .maximumSessions(1)
-                                                .expiredUrl("/login"))
-                                .logout(logout -> logout.permitAll())
-                                .build();
-        }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        @Bean
-        public UserDetailsService userDetailsService() {
-                UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("user")
-                                .password("123")
-                                .roles("USER")
-                                .build();
-                UserDetails admin = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("123")
-                                .roles("ADMIN")
-                                .build();
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = passwordEncoder();
 
-                return new InMemoryUserDetailsManager(user, admin);
-        }
+        UserDetails user = User.withUsername("user")
+            .password(encoder.encode("123"))
+            .roles("USER")
+            .build();
+
+        UserDetails admin = User.withUsername("admin")
+            .password(encoder.encode("123"))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+            .csrf(CsrfConfigurer::disable)
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/api/**", "/main-site/**", "/fontawesome/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .formLogin(formLogin -> formLogin
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error")
+                .permitAll())
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .permitAll())
+            .sessionManagement(sessionManagement -> sessionManagement
+                .maximumSessions(1)
+                .expiredUrl("/login"))
+            .build();
+    }
 }
